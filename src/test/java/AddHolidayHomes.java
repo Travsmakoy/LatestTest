c20434733245454367b040fd13d3dbb9d733cb53
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-08-28 17:08:05 +0400

    Final Push

---
+++ src/test/java/AddHolidayHomes.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class AddHolidayHomes extends Methods{
+    @Test
+    public void addHoliday(){
+        click(By.xpath("//button[normalize-space()='Projects']"));
+        click(By.xpath("//*[@id=\"__next\"]/div/nav/div/div/div[2]/ul[12]/button"));
+        click(By.xpath("//*[@id=\"__next\"]/div/nav/div/div/div[2]/ul[12]/div[1]/div/div/a/div/div/p"));
+    }
+}
--- src/test/java/SubsUpdateProject.java
+++ src/test/java/SubsUpdateProject.java
@@ -7,8 +7,4 @@ public class SubsUpdateProject extends Methods {
         click(By.xpath("//p[normalize-space()='Local Projects']"));
         URLvalidator("https://dashboard.aqaryint.com/dashboard/project/project_management/local");
     }
-    @Test(priority = 2)
-    public void UpdateSubs(){
-        click(By.xpath("(//div[@id='CompanyStatusCheckBox'])[1]"));
-    }
 }
