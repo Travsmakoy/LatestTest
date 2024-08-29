16662cf202d826c4ce81c3fec56f1074322838e9
Travsmakoy <57427376+Travsmakoy@users.noreply.github.com>
2024-08-29 10:53:35 +0400

    Final Push

--- src/test/java/startExecute.java
+++ src/test/java/startExecute.java
@@ -7,6 +7,5 @@ public class startExecute extends Methods {
     public void VerifyAqarySite(){
     driver.get("https://dashboard.aqaryint.com");
     URLvalidator("https://dashboard.aqaryint.com/");
-        Allure.step("Test1");
     }
 }
